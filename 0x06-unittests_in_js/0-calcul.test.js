e67ca854a9f4e603daa018ec49e8e4999bac3524
nokekip <kipnoke.gmail.com>
2024-05-03 01:11:46 +0300

    Basic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
+
+describe('calculateNumber', () => {
+    it('round of a', () => {
+        assert.equal(calculateNumber(2.0, 0), 2);
+        assert.equal(calculateNumber(2.1, 0), 2);
+        assert.equal(calculateNumber(2.9, 0), 3);
+    });
+
+    it('round of b', () => {
+        assert.equal(calculateNumber(0, 2.0), 2);
+        assert.equal(calculateNumber(0, 2.1), 2);
+        assert.equal(calculateNumber(0, 2.9), 3);
+    });
+
+    it('round of a and b', () => {
+        assert.equal(calculateNumber(2.1, 2.1), 4);
+        assert.equal(calculateNumber(2.5, 2.5), 5);
+        assert.equal(calculateNumber(2.4, 2.5), 5);
+        assert.equal(calculateNumber(2.9, 2.9), 6);
+    });
+})
